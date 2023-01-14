807c4c37cdf12541dbba2586045dc625081e8cfa
タイラー <66812754+Vader-7@users.noreply.github.com>
2023-01-14 01:34:09 -0300

    5

--- Python/5. Kyu-6.py
+++ Python/5. Kyu-6.py
@@ -11,6 +11,8 @@ Examples
 [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
 [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
 """
+
+
 def find_it(seq):
     for i in seq:
         if seq.count(i) % 2 != 0:
